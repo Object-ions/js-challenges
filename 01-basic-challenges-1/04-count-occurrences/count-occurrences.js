d999e8791266b904d7212928c58364cf37f04b3e
object-ions <moshikoatia@gmail.com>
2024-01-21 16:01:47 -0800

    Run and pass 4th challenge

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(string, charToCheck) {
+  let amount = 0;
+
+  for (var char in string) {
+    if (string[char] === charToCheck) {
+      amount++;
+    }
+  }
+  return amount;
+}
 
 module.exports = countOccurrences;
