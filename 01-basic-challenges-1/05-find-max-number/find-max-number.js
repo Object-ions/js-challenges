6d364e578894b142551469dc047f5ada0c6b4dd1
object-ions <moshikoatia@gmail.com>
2024-01-21 16:43:03 -0800

    add solution and run and pass 5th test

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+/*
+questions to ask -
+1. we are dealing with positive and negative numbers?
+2. how to handle if an array is empty?
+*/
+
+function findMaxNumber(numbersArr) {
+  let largestNumber = numbersArr[0];
+  for (let i = 0; i < numbersArr.length; i++) {
+    if (numbersArr[i] > largestNumber) {
+      largestNumber = numbersArr[i];
+    }
+  }
+  return largestNumber;
+}
 
 module.exports = findMaxNumber;
