a40b4b2e6ae43e4be4578d85a2c0d7ff88f66317
object-ions <moshikoatia@gmail.com>
2024-01-21 16:13:29 -0800

    pass another solution for challenge no 4

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,8 +1,23 @@
+/*
+//  Solution 1
 function countOccurrences(string, charToCheck) {
   let amount = 0;
 
   for (var char in string) {
     if (string[char] === charToCheck) {
+      amount++;
+    }
+  }
+  return amount;
+}
+*/
+
+// Solution 2
+function countOccurrences(string, char) {
+  let amount = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
       amount++;
     }
   }
