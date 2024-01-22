1313a2e1880876028508b254f9025a7eb3207c82
object-ions <moshikoatia@gmail.com>
2024-01-21 17:41:50 -0800

    add a 2nd solution for challenge 6 nd pass

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,5 @@
+// Solution 1
+/*
 function titleCase(str) {
   let modifiedStr = str.split('');
 
@@ -10,6 +12,20 @@ function titleCase(str) {
   }
 
   return modifiedStr.join('');
+}
+*/
+
+// Solution 2
+function titleCase(str) {
+  words = str.toLowerCase().split(' ');
+  // 'Hello world'
+  // -> ['hello', 'world'];
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    // -> ['Hello', 'World'];
+  }
+  return words.join(' ');
+  // -> 'Hello World';
 }
 
 module.exports = titleCase;
