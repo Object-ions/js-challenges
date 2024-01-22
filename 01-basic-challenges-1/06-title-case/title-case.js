b1edfadb3221b168165c343ea2976bf240cd81cb
object-ions <moshikoatia@gmail.com>
2024-01-21 17:28:11 -0800

    add solution and pass challenge 6

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,15 @@
-function titleCase() {}
+function titleCase(str) {
+  let modifiedStr = str.split('');
+
+  for (let i = 0; i < modifiedStr.length; i++) {
+    if (i === 0 || modifiedStr[i - 1] === ' ') {
+      modifiedStr[i] = modifiedStr[i].toUpperCase();
+    } else {
+      modifiedStr[i] = modifiedStr[i].toLowerCase();
+    }
+  }
+
+  return modifiedStr.join('');
+}
 
 module.exports = titleCase;
