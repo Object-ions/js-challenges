77448274af0ade7ff06688fe3c558196a825d29c
Object-ions <moshikoatia@gmail.com>
2024-01-23 22:33:09 -0800

    add another solution to challenge number 7

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,5 +1,17 @@
+// Solution 1
+/*
 function reverseString(string) {
   return string.split('').reverse().join('');
+}
+*/
+
+// Solution 2
+function reverseString(string) {
+  let reversed = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  return reversed;
 }
 
 module.exports = reverseString;
