d21da42c0b733c8b2e531a9cc0e5c4c0447f1e2a
Object-ions <moshikoatia@gmail.com>
2024-01-23 22:45:15 -0800

    add and pass (partial) challenge 8

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,20 @@
-function isPalindrome() {}
+// Questions to ask :
+/*
+1. 'empty string - true / false '?
+2. special chars? comma? period?
+*/
+
+function isPalindrome(string) {
+  // hello
+  // result = [h,e,l,l,o]
+  // result = [o,l,l,e,h]
+  // result = olleh
+  reversed = string.toLowerCase().split('').reverse().join('');
+  if (string === reversed) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = isPalindrome;
