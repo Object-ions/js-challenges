6b30801483a10bf26f02ceed2ddfacaf8726356b
Object-ions <moshikoatia@gmail.com>
2024-01-23 23:11:45 -0800

    add and pass solution to challenge 9

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,22 @@
-function countVowels() {}
+/*
+questions to ask:
+1. upper case / lower case ?
+2. handle empty str?
+*/
+
+function countVowels(str) {
+  let count = 0;
+  let vowels = ['a', 'e', 'o', 'i', 'u'];
+  // hello
+  // toCheck = [h,e,l,l,o]
+  let toCheck = str.trim().toLowerCase().split('');
+  toCheck.forEach((item) => {
+    if (vowels.includes(item)) {
+      count += 1;
+    }
+  });
+
+  return count;
+}
 
 module.exports = countVowels;
