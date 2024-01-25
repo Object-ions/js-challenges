c98bfb07698289c5b5e5ef19feab42b086f07931
Object-ions <moshikoatia@gmail.com>
2024-01-24 23:33:18 -0800

    add solution and pass challenge 11

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,22 @@
-function fizzBuzzArray() {}
+/*
+1. input > 0 ?
+1. input == number ?
+*/
+
+function fizzBuzzArray(number) {
+  let outputArr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 15 === 0) {
+      outputArr.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      outputArr.push('Buzz');
+    } else if (i % 3 === 0) {
+      outputArr.push('Fizz');
+    } else {
+      outputArr.push(i);
+    }
+  }
+  return outputArr;
+}
 
 module.exports = fizzBuzzArray;
