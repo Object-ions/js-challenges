e52b7b5c067bc3053c51da558210dd9609bf35a9
Object-ions <moshikoatia@gmail.com>
2024-01-24 23:50:38 -0800

    add a second solution for challenge 12

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -7,7 +7,7 @@
 // inputA = [1,2,3,4,5]
 // inputB = [1,3,5,7,9]
 // outputC = [1,3,5]
-
+/*
 function arrayIntersection(arrNumA, ArrNumB) {
   let arrNumC = [];
 
@@ -17,6 +17,20 @@ function arrayIntersection(arrNumA, ArrNumB) {
     }
   });
 
+  return arrNumC;
+}
+*/
+
+// Solution 2
+function arrayIntersection(arrNumA, arrNumB) {
+  const setA = new Set(arrNumA);
+  const arrNumC = [];
+
+  for (let item of arrNumB) {
+    if (setA.has(item)) {
+      arrNumC.push(item);
+    }
+  }
   return arrNumC;
 }
 
