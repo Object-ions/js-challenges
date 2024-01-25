1614291afb7c282acf5fd9ffed396399af72a13b
Object-ions <moshikoatia@gmail.com>
2024-01-24 23:14:44 -0800

    add another solution for challenge 10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -2,7 +2,8 @@
 1. type of input - int ? char? str? bool? 
 2. '    ' in account or no?
 */
-
+// Solution 1
+/*
 function removeDuplicates(arr) {
   // arr = [1,2,3,4,5,5,6,7,8,9]
   let noDuplications = [];
@@ -11,8 +12,15 @@ function removeDuplicates(arr) {
       noDuplications.push(item);
     }
   });
-
+  
   return noDuplications;
+}
+*/
+
+// Solution 2
+function removeDuplicates(arr) {
+  let noDuplications = new Set(arr);
+  return [...noDuplications]; // turn a set into an array with spread operator
 }
 
 module.exports = removeDuplicates;
