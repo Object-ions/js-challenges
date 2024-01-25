9dc4486c3657b7c7c98810a1acc6053f9582022b
Object-ions <moshikoatia@gmail.com>
2024-01-24 22:49:58 -0800

    add solution and pass challenge 10

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,18 @@
-function removeDuplicates() {}
+/*
+1. type of input - int ? char? str? bool? 
+2. '    ' in account or no?
+*/
+
+function removeDuplicates(arr) {
+  // arr = [1,2,3,4,5,5,6,7,8,9]
+  let noDuplications = [];
+  arr.forEach((item) => {
+    if (!noDuplications.includes(item)) {
+      noDuplications.push(item);
+    }
+  });
+
+  return noDuplications;
+}
 
 module.exports = removeDuplicates;
