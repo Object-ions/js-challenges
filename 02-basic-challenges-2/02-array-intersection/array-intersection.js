1984a859ac00ad6b3fda483b0f237de350f67298
Object-ions <moshikoatia@gmail.com>
2024-01-24 23:40:52 -0800

    add and pass challenge 12

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,23 @@
-function arrayIntersection() {}
+/*
+1. input > 0 ?
+2. input == int ?
+*/
+
+// Examples:
+// inputA = [1,2,3,4,5]
+// inputB = [1,3,5,7,9]
+// outputC = [1,3,5]
+
+function arrayIntersection(arrNumA, ArrNumB) {
+  let arrNumC = [];
+
+  arrNumA.forEach((item) => {
+    if (ArrNumB.includes(item)) {
+      arrNumC.push(item);
+    }
+  });
+
+  return arrNumC;
+}
 
 module.exports = arrayIntersection;
