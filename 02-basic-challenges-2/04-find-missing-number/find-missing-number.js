f34c198782c2e57f9459bc2e988599b36a601c6d
Object-ions <moshikoatia@gmail.com>
2024-01-25 18:35:50 -0800

    add solution and pass challenge 14

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,29 @@
-function findMissingNumber() {}
+/*
+1. handle empty arr? -> return 1
+2. starting from 0? 1 ?
+2. only arr?
+3. only one number missing? (return is int and not array)
+4. is there always be a missing number? what if no missing number?
+*/
+
+// [1,2,3,5] -> 4
+
+function findMissingNumber(numArr) {
+  let missingNumber;
+  let newArray = [];
+  let length = numArr.length;
+
+  for (let i = 1; i <= length + 1; i++) {
+    newArray.push(i);
+  }
+
+  newArray.forEach((item) => {
+    if (!numArr.includes(item)) {
+      missingNumber = item;
+    }
+  });
+
+  return missingNumber;
+}
 
 module.exports = findMissingNumber;
