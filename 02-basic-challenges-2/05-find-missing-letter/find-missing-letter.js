348f5bfc58123bee4b20b0b010f45d794e8498d3
Object-ions <moshikoatia@gmail.com>
2024-01-26 18:14:00 -0800

    add (portion) solution for challenge 15, pass first test.

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,51 @@
-function findMissingLetter() {}
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+function findMissingLetter(stringArr) {
+  let solution = '';
+  // stringArr = [a,b,c,e] -> d
+  let length = stringArr.length; // 4
+  const startingLetter = stringArr[0]; // a
+  let arrayPotion = alphabet.slice(0, length + 1); // [a,b,c,d,e]
+
+  console.log(arrayPotion);
+
+  // compare srtingArr ?== arrayPotion;
+
+  for (let item of arrayPotion) {
+    if (!stringArr.includes(item)) {
+      solution = item;
+    }
+  }
+  console.log(solution);
+
+  return solution;
+}
 
 module.exports = findMissingLetter;
