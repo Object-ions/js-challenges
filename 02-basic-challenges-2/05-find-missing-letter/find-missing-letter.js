c69805419f4372a0297b3b850e93f704fd698053
Object-ions <moshikoatia@gmail.com>
2024-01-26 18:38:09 -0800

    pass test number 2 in challenge 15

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,30 +1,32 @@
+// case sensitive? A vs. a
+
 const alphabet = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
+  'a', // (1)
+  'b', // (2)
+  'c', // (3)
+  'd', // (4)
+  'e', // (5)
+  'f', // (6)
+  'g', // (7)
+  'h', // (8)
+  'i', // (9)
+  'j', // (10)
+  'k', // (11)
+  'l', // (12)
+  'm', // (13)
+  'n', // (14)
+  'o', // (15)
+  'p', // (16)
+  'q', // (17)
+  'r', // (18)
+  's', // (19)
+  't', // (20)
+  'u', // (21)
+  'v', // (22)
+  'w', // (23)
+  'x', // (24)
+  'y', // (25)
+  'z', // (26)
 ];
 
 function findMissingLetter(stringArr) {
@@ -32,11 +34,14 @@ function findMissingLetter(stringArr) {
   // stringArr = [a,b,c,e] -> d
   let length = stringArr.length; // 4
   const startingLetter = stringArr[0]; // a
-  let arrayPotion = alphabet.slice(0, length + 1); // [a,b,c,d,e]
+  console.log(startingLetter);
+
+  let startCut = alphabet.indexOf(startingLetter);
+  console.log(startCut);
+
+  let arrayPotion = alphabet.slice(startCut, startCut + length + 1); // [a,b,c,d,e]
 
   console.log(arrayPotion);
-
-  // compare srtingArr ?== arrayPotion;
 
   for (let item of arrayPotion) {
     if (!stringArr.includes(item)) {
--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.test.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.test.js
@@ -2,7 +2,7 @@ const findMissingLetter = require('./find-missing-letter');
 
 test('Find Missing Letter', () => {
   expect(findMissingLetter(['a', 'b', 'c', 'e'])).toBe('d');
-  expect(findMissingLetter(['X', 'Z'])).toBe('Y');
+  expect(findMissingLetter(['x', 'z'])).toBe('y');
   expect(findMissingLetter(['m', 'n', 'o', 'q', 'r'])).toBe('p');
   expect(findMissingLetter(['F', 'G', 'H', 'J'])).toBe('I');
 });
