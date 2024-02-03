e9874dc6899582f5616adcd28366369ade21a452
Object-ions <moshikoatia@gmail.com>
2024-02-02 23:17:18 -0800

    Add solution to challenge 19

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,18 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbersArr) {
+  // numbersArr = [1,2,3,4,5,6,7,8,9,0] => '(123) 456-7890'
+  // numbersArr[0-2] add () => (123)
+  let resultArr = [];
+  resultArr = numbersArr.slice(0, 3);
+  resultArr.unshift('(');
+  resultArr.push(') ');
+  resultArr.push(numbersArr.slice(3, 6));
+  resultArr.push('-');
+  resultArr.push(numbersArr.slice(6, 9));
+
+  console.log('result array: ', resultArr);
+  let result = resultArr.join(',');
+  let resultNoCommas = result.replace(/,/g, '');
+  console.log('after join', resultNoCommas);
+}
 
 module.exports = formatPhoneNumber;
