6bd40e6ad28243608a0a174b597f512dd32c4c37
Object-ions <moshikoatia@gmail.com>
2024-02-02 23:28:43 -0800

    Add another solution for challenge 19

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,5 @@
+/*
+// Solution 1
 function formatPhoneNumber(numbersArr) {
   // numbersArr = [1,2,3,4,5,6,7,8,9,0] => '(123) 456-7890'
   // numbersArr[0-2] add () => (123)
@@ -11,6 +13,15 @@ function formatPhoneNumber(numbersArr) {
 
   let result = resultArr.join('');
   return result;
+}
+*/
+
+// Solution 2
+function formatPhoneNumber(numbersArr) {
+  let areaCode = numbersArr.slice(0, 3).join('');
+  let prefix = numbersArr.slice(3, 6).join('');
+  let lineNumber = numbersArr.slice(6).join('');
+  return `(${areaCode}) ${prefix}-${lineNumber}`;
 }
 
 module.exports = formatPhoneNumber;
