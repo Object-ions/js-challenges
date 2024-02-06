5f980b786d29c7cf43d78cea99c37137f2b56ee6
Object-ions <moshikoatia@gmail.com>
2024-02-05 18:52:02 -0800

    add a second solution for challenge #20

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,6 +1,29 @@
+/*
 function validateEmail(email) {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailRegex.test(email);
+}
+*/
+
+function validateEmail(email) {
+  //  user@domain.com
+  if (!email.includes('@')) {
+    return false;
+  }
+
+  const [user, domain] = email.split('@');
+
+  //  user@domain.com
+  if (user.length === 0 || domain.length < 3) {
+    return false;
+  }
+
+  const domainEx = domain.split('.');
+  if (domainEx.length < 2) {
+    return false;
+  }
+
+  return true;
 }
 
 module.exports = validateEmail;
