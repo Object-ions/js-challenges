dc41fe368af163debd8fe04c4b9d5815b448335d
object-ions <moshikoatia@gmail.com>
2024-02-15 23:19:26 -0800

    add solution to challenge 22

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,12 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+  const totalSales = products.reduce((sum, product) => {
+    sum + product.price * product.quantity
+  } , 0);
+
+  const taxAmount = (totalSales * taxRate) / 100;
+
+  const totalSalesWithTax = taxAmount + totalSales
+  return parseInt(totalSalesWithTax.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
