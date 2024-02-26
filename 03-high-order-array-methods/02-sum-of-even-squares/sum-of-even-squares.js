4276a8f20708e3fa2e9ebcb02b360a2ec49fe9a1
Object-ions <moshikoatia@gmail.com>
2024-02-25 22:34:33 -0800

    Refactor sumOfEvenSquares function to use a more descriptive variable name

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,8 +1,9 @@
 function sumOfEvenSquares(numbers) {
-  const evenSquares = numbers
+  const evenSquare = numbers
     .filter((num) => num % 2 === 0)
     .map((num) => num ** 2)
-    .reduce((sum, square) => sum + square, 0);
-  return evenSquares;
+    .reduce((acc, num) => acc + num, 0);
+
+  return evenSquare;
 }
 module.exports = sumOfEvenSquares;
