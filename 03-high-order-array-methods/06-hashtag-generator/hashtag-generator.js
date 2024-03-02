8844ef0228f95c4fd7eb4f93067e0c4971058a0d
Object-ions <moshikoatia@gmail.com>
2024-03-01 23:52:49 -0800

    Add a second solution for challenge 24 using reducer

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,5 @@
+/*
+// Solution 1
 function generateHashtag(str) {
   if (str.trim() === '') {
     return false;
@@ -12,6 +14,16 @@ function generateHashtag(str) {
   const hashtag = '#' + capitalizeWords.join('');
 
   return hashtag.length > 140 ? false : hashtag;
+}
+*/
+
+// Solution 2
+function generateHashtag(str) {
+  const hashtag = str.split(/\s+/).reduce((acc, word) => {
+    return acc + word.charAt(0).toUpperCase() + word.substring(1);
+  }, '#');
+
+  return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
 }
 
 module.exports = generateHashtag;
