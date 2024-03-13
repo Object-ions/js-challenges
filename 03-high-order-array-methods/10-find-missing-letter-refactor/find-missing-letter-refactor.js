d3e8cef858b284ba871e068497b3ecc5864744f2
object-ions <moshikoatia@gmail.com>
2024-03-12 23:07:30 -0400

    add solution for challenge 28

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,19 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let start = arr[0].charCodeAt(0);
+
+  const missingCharCode = arr
+    .map((char) => char.charCodeAt(0)) // convert each character to its character code
+    .find((current) => {
+       //  if the current character code is more than one greater than the previous character code then return true
+       if (current - start > 1) {
+         return true;
+       }
+       // otherwise set `start` to the current character code and return false
+       start = current;
+       return false;
+    })
+  // will return the char from the code
+  return missingCharCode? String.fromCharCode(missingCharCode - 1) : ''; 
+}
 
 module.exports = findMissingLetter;
