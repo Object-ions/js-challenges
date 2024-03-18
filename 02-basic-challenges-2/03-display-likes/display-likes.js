1dbe940589dbeae104a47d07f616d38d5052e57c
Object-ions <moshikoatia@gmail.com>
2024-03-18 13:27:21 -0700

    re-solve challenge 13 with psudo solution

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -33,12 +33,30 @@ function displayLikes(namesArr) {
 */
 
 function displayLikes(arrayOfNames) {
-  // loop through the indexes if the array
+  // switch conditions
   // return the name in the current the index with 'like this' depending on the amount of the indexes in the array
-  // if the array is empty return 'no one likes this'
-  // if the array has 1 item return 'arrayOfNames[i] likes this'
-  // if the array has 2 items return 'arrayOfNames[0] and arrayOfNames[1] likes this'
-  // if the array has 3 items return 'arrayOfNames[0] and arrayOfNames[1] and arrayOfNames[2] likes this'
-  // if the array has 4 and more items return 'arrayOfNames[0] and arrayOfNames[1] and arrayOfNames.length - 2 others like this'
+  switch (arrayOfNames.length) {
+    // if the array is empty return 'no one likes this'
+    case 0:
+      return 'no one likes this';
+
+    // if the array has 1 item return 'arrayOfNames[i] likes this'
+    case 1:
+      return `${arrayOfNames[0]} likes this`;
+
+    // if the array has 2 items return 'arrayOfNames[0] and arrayOfNames[1] likes this'
+    case 2:
+      return `${arrayOfNames[0]} and ${arrayOfNames[1]} like this`;
+
+    // if the array has 3 items return 'arrayOfNames[0] and arrayOfNames[1] and arrayOfNames[2] likes this'
+    case 3:
+      return `${arrayOfNames[0]}, ${arrayOfNames[1]} and ${arrayOfNames[2]} like this`;
+
+    // if the array has 4 and more items return 'arrayOfNames[0] and arrayOfNames[1] and arrayOfNames.length - 2 others like this'
+    default:
+      return `${arrayOfNames[0]}, ${arrayOfNames[1]} and ${
+        arrayOfNames.length - 2
+      } others like this`;
+  }
 }
 module.exports = displayLikes;
