021feaf2fc09388f7d952e073236a9ae12d0d78d
Object-ions <moshikoatia@gmail.com>
2024-03-18 11:37:24 -0700

    add psudo solution to challenge 11

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -2,7 +2,7 @@
 1. input > 0 ?
 1. input == number ?
 */
-
+/*
 function fizzBuzzArray(number) {
   let outputArr = [];
   for (let i = 1; i <= number; i++) {
@@ -17,6 +17,19 @@ function fizzBuzzArray(number) {
     }
   }
   return outputArr;
+}
+*/
+
+function fizzBuzzArray(number) {
+  // initialize an empty array
+  // check if the number divides by 15
+  // if it does add 'FizzBuzz' to the array (end)
+  // check if the number divides by 5
+  // if it does add 'Buzz' to the array (end)
+  // check if the number divides by 3
+  // if it does add 'Fizz' to the array (end)
+  // else add the number itself
+  // return the array with the result
 }
 
 module.exports = fizzBuzzArray;
