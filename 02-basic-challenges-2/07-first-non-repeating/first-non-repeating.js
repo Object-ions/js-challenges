1da837c5330f008abe21f1b57b5d4ade03ae47a9
Object-ions <moshikoatia@gmail.com>
2024-03-26 11:14:35 -0700

    re-solve challenge 17 with psudo

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -15,11 +15,29 @@
 
 function findFirstNonRepeatingCharacter(s) {
   // create a char map
+  const charMap = {};
+
   // iterate through s
   // assign (create the object) the chars in the map with the count
-  // loop to check if a char has assigned only once in the char map
-  // return this char
+
+  /*
+  if the character is not there in charMap treat its count as 0 and then add 1 to it, (effectively initializing it with a count of 1). If it's already there just add 1 to its current count.
+  */
+
+  for (let char of s) {
+    charMap[char] = (charMap[char] || 0) + 1;
+  }
+
+  // loop through the completed object check if a char has assigned only once in the char map
+  for (let char in charMap) {
+    if (charMap[char] === 1) {
+      // return this char
+      return char;
+    }
+  }
+
   // if none repeated char return 'null'
+  return null;
 }
 
 module.exports = findFirstNonRepeatingCharacter;
