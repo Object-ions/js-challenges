a830c6628638aac5bf40d129ba07f098e4df31cb
Object-ions <moshikoatia@gmail.com>
2024-03-26 10:55:55 -0700

    add psudo solution for challenge 17

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,16 +1,25 @@
-function findFirstNonRepeatingCharacter(str) {
-  const charCount = {};
+// function findFirstNonRepeatingCharacter(str) {
+//   const charCount = {};
 
-  for (const char of str) {
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
-  for (const char of str) {
-    if (charCount[char] === 1) {
-      return char;
-    }
-  }
+//   for (const char of str) {
+//     charCount[char] = (charCount[char] || 0) + 1;
+//   }
+//   for (const char of str) {
+//     if (charCount[char] === 1) {
+//       return char;
+//     }
+//   }
 
-  return null;
+//   return null;
+// }
+
+function findFirstNonRepeatingCharacter(s) {
+  // create a char map
+  // iterate through s
+  // assign (create the object) the chars in the map with the count
+  // loop to check if a char has assigned only once in the char map
+  // return this char
+  // if none repeated char return 'null'
 }
 
 module.exports = findFirstNonRepeatingCharacter;
