e0405f79f365ebee365f3736fd27f18a16a81fbd
Object-ions <moshikoatia@gmail.com>
2024-03-28 12:02:17 -0700

    re-solve challenge 18

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,4 @@
+/*
 function rollDice() {
   return Math.floor(Math.random() * 6 + 1);
 }
@@ -24,6 +25,42 @@ function diceGameSimulation(userInput) {
 
   return results;
   // { dice1: dice1, dice2: dice2, sum: sum, result: result}
+}
+*/
+
+/*
+
+const game = {
+  dice1: number,
+  dice2: number,
+  sum: dice1 + dice2,
+  result: "win" || "loose" || "roll again"
+};
+*/
+const gameArray = [];
+
+function rollDice() {
+  return Math.floor(Math.random() * 6 + 1);
+}
+
+function diceGameSimulation(numSimulations) {
+  for (let i = 0; i < numSimulations; i++) {
+    const game = {};
+    game.dice1 = rollDice();
+    game.dice2 = rollDice();
+    game.sum = game.dice1 + game.dice2;
+
+    if (game.sum === 7 || game.sum === 11) {
+      game.result = 'win';
+    } else if (game.sum === 2 || game.sum === 3 || game.sum === 12) {
+      game.result = 'lose';
+    } else {
+      game.result = 'roll again';
+    }
+
+    gameArray.push(game);
+  }
+  return gameArray;
 }
 
 module.exports = diceGameSimulation;
