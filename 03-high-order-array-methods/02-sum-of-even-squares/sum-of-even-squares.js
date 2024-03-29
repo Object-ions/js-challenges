ea5caf44e8ea55c4a5d775321436d179815d075c
Object-ions <moshikoatia@gmail.com>
2024-03-29 16:54:19 -0700

    add psudo solution for challenge 31

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,4 @@
+/*
 function sumOfEvenSquares(numbers) {
   const evenSquare = numbers
     .filter((num) => num % 2 === 0)
@@ -6,4 +7,13 @@ function sumOfEvenSquares(numbers) {
 
   return evenSquare;
 }
+*/
+
+function sumOfEvenSquares(numbers) {
+  // first we will filter the even numbers
+  // get each of them their squared result
+  // add them together
+  // return that result
+}
+
 module.exports = sumOfEvenSquares;
