7ab22b43857d9d710252ac86c4022e790a7c4221
Object-ions <moshikoatia@gmail.com>
2024-03-31 11:55:26 -0700

    add psudo solution for challenge 33

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -25,6 +25,7 @@ function highestScoringWord(str) {
 */
 
 // Solution 2
+/*
 function highestScoringWord(str) {
   const words = str.split(' ');
 
@@ -39,6 +40,24 @@ function highestScoringWord(str) {
   const highestIndex = scores.indexOf(highestScore);
 
   return words[highestIndex];
+}
+*/
+
+function highestScoringWord(str) {
+  // create highestScore to store the score of the highest scoring word
+  // create highestWord to store the highest scoring word
+  // create a charMap using a loop to assign scores to letters
+  /*
+    a: 1
+    b: 2
+    c: 3
+  */
+  // break str into an arrayOfWords
+  // for each word in the array calculate it's score
+  // if the score of the current word is greater than the highestScore
+  // update the highestScore with the new highest score
+  // update the highestWord with the new highest word
+  // return highestWord
 }
 
 module.exports = highestScoringWord;
