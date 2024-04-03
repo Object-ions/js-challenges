d4a7ea1839ea04fced5f6e22d4e58e30dcbc6eb1
Object-ions <moshikoatia@gmail.com>
2024-04-03 14:16:00 -0700

    re-solve challenge 35 with psudo solution

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -18,13 +18,40 @@ function validAnagrams(str1, str2) {
 
 function validAnagrams(str1, str2) {
   // Make both words lowercase
+  str1Lowercase = str1.toLowerCase();
+  str2Lowercase = str2.toLowerCase();
+
   // If word lengths don't match, return false
+  if (str1Lowercase.length !== str2Lowercase.length) {
+    return false;
+  }
+
   // Create empty map for letters in word1
+  const charMap1 = {};
+
   // Fill map with each letter from word1 and count how many times each appears
+  for (let char of str1Lowercase) {
+    charMap1[char] = (charMap1[char] || 0) + 1;
+  }
+
   // Go through each letter in word2
-  // If letter is in map, decrease its count
-  // If letter not in map or count goes below 0, return false
+  for (let char of str2Lowercase) {
+    // If letter not in map or count goes below 0, return false
+    if (!charMap1[char]) {
+      return false;
+    } else {
+      // If letter is in map, decrease its count
+      charMap1[char] -= 1;
+    }
+  }
   // If all counts in map are zero, return true
+  for (let char in charMap1) {
+    if (charMap1[char] !== 0) {
+      return false;
+    }
+  }
+
   // Otherwise, return false
+  return true;
 }
 module.exports = validAnagrams;
