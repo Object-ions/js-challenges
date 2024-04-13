e430987ae8305f059f7970acd76d343b1eefdb95
object-ions <moshikoatia@gmail.com>
2024-04-12 21:35:03 -0700

    re-solve challenge 37 including psudo solution

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -15,9 +15,24 @@ const isValidIPv4 = (str) => {
 
 function isValidIPv4(string) {
   // break string into array by '.'
+  const octets = string.split('.');
+  
   // run a check if array length is 4
-  // return false if not 4
+  if (octets.length !== 4) {
+    // return false if not 4
+    return false;
+  }
+  
   // run a check on each item in the array
-  // if array item not between 0 and 255 return false
+  for (let item of octets) {
+    const num = parseInt(item);
+    // Check if the item fails any criteria
+    if (!(num >= 0 && num <= 255 && item === num.toString())) {
+      return false;  // Return false if invalid
+    }
+  }
+
+  return true;  // Return true if pass all tests
 }
+
 module.exports = isValidIPv4;
