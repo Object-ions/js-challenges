a340f1a435b276cd89106614bd452bf19a14676d
Object-ions <moshikoatia@gmail.com>
2024-05-26 16:37:03 -0700

    And and pass challenge number 39 with psudo

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -16,9 +16,22 @@ function validatePassword(password) {
 
 function validatePassword(password) {
   // check if the length is valid
+  const isLengthValid = password.length >= 8 ? true : false;
+
   // check if the password contains uppercase char
+  const isUppercase = password
+    .split('')
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
   // check if the password contains lowercase char
+  const isLowercase = password
+    .split('')
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
   // check if the password contains min of one digit
+  const isInt = password.split('').some((char) => !isNaN(char));
+
   // return true if all tests are true : false
+  return isLengthValid && isUppercase && isLowercase && isInt;
 }
 module.exports = validatePassword;
