899f1941a64a6bd5cb198930b3606f567a11197d
object-ions <moshikoatia@gmail.com>
2024-08-27 21:50:01 -0400

    resolve challenge

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -37,30 +37,79 @@ const game = {
   result: "win" || "loose" || "roll again"
 };
 */
-const gameArray = [];
+// const gameArray = [];
 
-function rollDice() {
-  return Math.floor(Math.random() * 6 + 1);
+// function rollDice() {
+//   return Math.floor(Math.random() * 6 + 1);
+// }
+
+// function diceGameSimulation(numSimulations) {
+//   for (let i = 0; i < numSimulations; i++) {
+//     const game = {};
+//     game.dice1 = rollDice();
+//     game.dice2 = rollDice();
+//     game.sum = game.dice1 + game.dice2;
+
+//     if (game.sum === 7 || game.sum === 11) {
+//       game.result = 'win';
+//     } else if (game.sum === 2 || game.sum === 3 || game.sum === 12) {
+//       game.result = 'lose';
+//     } else {
+//       game.result = 'roll again';
+//     }
+
+//     gameArray.push(game);
+//   }
+//   return gameArray;
+// }
+
+
+// RULES:
+// if 7 or 11 are rolled: 'win'
+// if 2/3/12 rolled: 'lose'
+// else: 'roll again'
+
+// function signature
+// param {number} numSimulations - the number of times to simulate the dice game
+// returns {Array} - an array of objects simulation result.
+
+// diceGameSimulation(3)
+
+/*
+  [
+    { dice1: 1, dice2: 5, sum: 6, result: 'roll again' }
+  ]
+*/
+
+const rollDice = () => {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-function diceGameSimulation(numSimulations) {
-  for (let i = 0; i < numSimulations; i++) {
-    const game = {};
-    game.dice1 = rollDice();
-    game.dice2 = rollDice();
-    game.sum = game.dice1 + game.dice2;
+const diceGameSimulation = (numSimulations) => {
+  const results = [];
 
-    if (game.sum === 7 || game.sum === 11) {
-      game.result = 'win';
-    } else if (game.sum === 2 || game.sum === 3 || game.sum === 12) {
-      game.result = 'lose';
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+    const sum = dice1 + dice2;
+    let result = '';
+
+    if (sum === 7 || sum === 11) {
+      result = 'win';
+    } else if (sum === 2 || sum === 3|| sum === 12) {
+      result = 'lose';
     } else {
-      game.result = 'roll again';
+      result = 'roll again';
     }
 
-    gameArray.push(game);
+    results.push( {
+      result, 
+      dice1,
+      dice2,
+      sum,
+    });
   }
-  return gameArray;
+  return results;
 }
 
 module.exports = diceGameSimulation;
